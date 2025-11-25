e845052428ec0db1b1f28c77862033a34dd57d82
lrndar <valean_daria@yahoo.com>
2025-11-25 11:56:00 +0200

    Lab 8

---
+++ Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace ValeanDariaLab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
